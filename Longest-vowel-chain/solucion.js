978db1c4d5171ca87e9dd23afac8cee285189079
Jonathan David Mendez Zuñiga <82689439+Jonathan-DT@users.noreply.github.com>
2023-09-20 09:17:14 -0600

    solucion actualizada

--- Longest-vowel-chain/solucion.js
+++ Longest-vowel-chain/solucion.js
@@ -1,21 +1,3 @@
 function solve(s){
-    let a = s.split("")
-    let cont=0, hi=0;
-    
-    for(i=0;i<a.length;i++){
-      
-      if(a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u'){
-        cont++;
-        
-        if(cont>hi){
-          hi=cont;  
-        }
-        
-      }else{
-        cont =0;
-      }
-      
-    }
-   
-   return hi;
+    return Math.max(...s.match(/[aeiou]+/g).map( x => x.length));
   }
